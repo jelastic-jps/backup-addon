17970f9ac2879cab655dcc485586a44989a9c7dd
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-03-03 20:52:16 +0200

    Update backup-manager.js

--- scripts/backup-manager.js
+++ scripts/backup-manager.js
@@ -74,7 +74,8 @@ function BackupManager(config) {
 		'RESTIC_PASSWORD=%(envName) restic -r /opt/backup snapshots || RESTIC_PASSWORD=%(envName) restic init -r /opt/backup',
 		'DUMP_NAME=$(date "+%F-%H-%M-%S")',
 		'for i in DB_HOST DB_USER DB_PASSWORD DB_NAME; do declare "${i}"=$(cat %(appPath)/wp-config.php |grep ${i}|awk \'{print $3}\'|tr -d "\'"); done',
-		'mysqldump -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} --force --single-transaction --quote-names --opt --databases --compress --column-statistics=0 > ${DB_NAME}.sql',
+	        '[[ "${MARIADB_VERSION%.*}" == "10.3" ]] && COL_STAT="--column-statistics=0" || COL_STAT=""',
+		'mysqldump -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} --force --single-transaction --quote-names --opt --databases --compress ${COL_STAT} > ${DB_NAME}.sql',
 		'RESTIC_PASSWORD=%(envName) restic -r /opt/backup backup --tag ${DUMP_NAME} %(appPath) ~/${DB_NAME}.sql',
 		'RESTIC_PASSWORD=%(envName) restic forget -r /opt/backup --keep-last %(backupCount) --prune'
             ], {
