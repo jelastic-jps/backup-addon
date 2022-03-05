a5793b647d5df8d2393464edbb8ced9e09950ab4
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-03-05 12:48:24 +0200

    Update backup-manager.js

--- scripts/backup-manager.js
+++ scripts/backup-manager.js
@@ -91,17 +91,25 @@ function BackupManager(config) {
     };
 
     me.addMountForBackup = function addMountForBackup() {
-	var allMounts = jelastic.env.file.GetMountPoints(config.envName, session, config.backupExecNode).array;
-	for (var i = 0, n = allMounts.length; i < n; i++) {
-            if (mountssss[i].sourcePath == "/data/" + config.envName && allMounts[i].path == "/opt/backup" && allMounts[i].name == "WP backup" && allMounts[i].type == "INTERNAL") {
-		jelastic.env.file.RemoveMountPointById(config.envName, session, config.backupExecNode, "/opt/backup");
-            }
-        }
+	//var allMounts = jelastic.env.file.GetMountPoints(config.envName, session, config.backupExecNode).array;
+	//for (var i = 0, n = allMounts.length; i < n; i++) {
+        //    if (mountssss[i].sourcePath == "/data/" + config.envName && allMounts[i].path == "/opt/backup" && allMounts[i].name == "WP backup" && allMounts[i].type == "INTERNAL") {
+	//	jelastic.env.file.RemoveMountPointById(config.envName, session, config.backupExecNode, "/opt/backup");
+        //    }
+        //}
+	[ me.removeMountForBackup ],
         return jelastic.env.file.AddMountPointById(config.envName, session, config.backupExecNode, "/opt/backup", 'nfs4', null, '/data/' + config.envName, config.storageNodeId, 'WP backup', false); 
     }
 
     me.removeMountForBackup = function removeMountForBackup() {
-        return jelastic.env.file.RemoveMountPointById(config.envName, session, config.backupExecNode, "/opt/backup");
+	var allMounts = jelastic.env.file.GetMountPoints(config.envName, session, config.backupExecNode).array;
+	for (var i = 0, n = allMounts.length; i < n; i++) {
+            if (mountssss[i].sourcePath == "/data/" + config.envName && allMounts[i].path == "/opt/backup" && allMounts[i].name == "WP backup" && allMounts[i].type == "INTERNAL") {
+		return jelastic.env.file.RemoveMountPointById(config.envName, session, config.backupExecNode, "/opt/backup");
+            } else {
+		return { result: 0 };
+	    }
+        }
     }
 
     me.checkEnvStatus = function checkEnvStatus() {
