c7cdcc5b830c65ea1a6d2532d8d7c49692ef475c
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-03-18 17:11:47 +0200

    Update getBackups.sh

--- scripts/getBackups.sh
+++ scripts/getBackups.sh
@@ -5,7 +5,7 @@ if [ $# -eq 0 ]; then
     OUTPUT_JSON="{\"result\": 0, \"envs\": [${ENV_LIST}"
 else
     ENV_NAME=$1
-    [ -d "/data/$1" ] && BACKUP_LIST=$(RESTIC_PASSWORD="$1" restic -r /data/$1 snapshots|awk '{print $5}'|grep -o [0-9-]*|awk '{print "\""$1"\""}'|tr '\n' ',')
+    [ -d "/data/$1" ] && BACKUP_LIST=$(RESTIC_PASSWORD="$1" restic -r /data/$1 snapshots|awk '{print $5}'|grep -o [0-9_-]*|awk '{print "\""$1"\""}'|tr '\n' ',')
     OUTPUT_JSON="{\"result\": 0, \"backups\": [${BACKUP_LIST}"
     [ -n "${BACKUP_LIST}" ] && OUTPUT_JSON=${OUTPUT_JSON::-1}
 fi
